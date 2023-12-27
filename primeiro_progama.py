d37cc861998bb332313b41c808f591cb40215c51
Lucas <136651806+Lucasholt124@users.noreply.github.com>
2023-12-27 14:01:03 -0300

    Add files via upload

---
+++ primeiro_progama.py
@@ -0,0 +1,16 @@
+import requests
+import json
+import time
+import datetime
+
+while True:
+    requisicao = requests.get('http://api.promasters.net.br/cotacao/v1/valores')
+    cotacao =json.loads(requisicao.text)
+
+
+
+    print('#### COTAÇÃO ####')
+    print('Dòlar:', cotacao['valores']['USD']['valor'])
+    print('Euro:', cotacao['valores']['EUR']['valor'])
+    print('BTC', cotacao['valores']['BTC']['valor'])
+    time.sleep(5)
---
+++ programa_2.py
@@ -0,0 +1,10 @@
+nome = input(' Escreva seu nome: ')
+cpf = input(' Escreva seu cpf: ')
+endereço = input(' Escreva seu endereço: ')
+idade = input(' Escreva sua idade: ')
+altura = input(' Escreva sua altura: ')
+telefone= input(' Escreva seu telefone: ')
+
+
+print('seu nome é', nome, 'seu cpf é', cpf, 'sua residencia:', endereço, 'a idade é' ,
+      idade, 'a altura é' , altura, 'e seu telefone é', telefone)
